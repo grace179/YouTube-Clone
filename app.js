9cdd2af19d0f65469f023c59b144310d9fb1a937
grace179 <72989367+grace179@users.noreply.github.com>
2021-02-28 00:07:19 +0900

    webpack

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
-import express from 'express';
-import morgan from 'morgan';
-import helmet from 'helmet';
-import cookieParser from 'cookie-parser';
-import bodyParser from 'body-parser';
-import userRouter from './routers/userRouter';
-import videoRouter from './routers/videoRouter';
-import globalRouter from './routers/globalRouter';
-import routes from './routes';
-import { localsMiddleware } from './middlewares';
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
 
-app.use( helmet({ contentSecurityPolicy: false })); 
+app.use(helmet({ contentSecurityPolicy: false }));
 app.set("view engine", "pug");
 app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("static"));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
