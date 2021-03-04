f740bacc4bb8ba83d7d76af18236fe3ed82a2abd
grace179 <72989367+grace179@users.noreply.github.com>
2021-03-04 16:06:39 +0900

    GitHub Login

--- passport.js
+++ passport.js
@@ -1,7 +1,26 @@
 import passport from "passport";
+import GithubStrategy from "passport-github";
 import User from "./models/User";
+import { githubLoginCallback } from "./controllers/userController";
+import routes from "./routes";
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.use(
+  new GithubStrategy(
+    {
+      clientID: process.env.GH_ID,
+      clientSecret: process.env.GH_SECRET,
+      callbackURL: "http://localhost:4000/auth/github/callback",
+    },
+    githubLoginCallback
+  )
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
