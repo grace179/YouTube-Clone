9bafa3f1c790bfa3b7a94da5c7a1426728d512f9
grace179 <72989367+grace179@users.noreply.github.com>
2021-03-06 14:38:24 +0900

    add videoPlayer

--- controllers/userController.js
+++ controllers/userController.js
@@ -111,8 +111,16 @@ export const logout = (req, res) => {
   // res.render("logOut", { pageTitle: "LogOut"})
 };
 
-export const getMe = (req, res) => {
-  res.render("userDetail", { pageTitle: "User Detail", user: req.user });
+export const getMe = async (req, res) => {
+  const {
+    params: { id },
+  } = req;
+  try {
+    const user = await User.findById(req.user.id).populate("videos");
+    res.render("userDetail", { pageTitle: "User Detail", user: user });
+  } catch (error) {
+    res.redirect(routes.home);
+  }
 };
 
 export const users = (req, res) => res.render("users", { pageTitle: "Users" });
@@ -122,7 +130,8 @@ export const userDetail = async (req, res) => {
     params: { id },
   } = req;
   try {
-    const user = await User.findById(id);
+    const user = await User.findById(id).populate("videos");
+    console.log(user);
     res.render("userDetail", { pageTitle: "User Detail", user });
   } catch (error) {
     res.redirect(routes.home);
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -61,7 +61,7 @@ export const postUpload = async (req, res) => {
     description,
     creator: req.user.id,
   });
-  console.log(newVideo);
+  // console.log(newVideo);
   req.user.videos.push(newVideo.id);
   req.user.save();
   res.redirect(routes.videoDetail(newVideo.id));
@@ -74,7 +74,7 @@ export const videoDetail = async (req, res) => {
 
   try {
     const video = await Video.findById(id).populate("creator");
-    console.log(video);
+    // console.log(video);
 
     res.render("videoDetail", {
       pageTitle: video.title,
@@ -92,10 +92,14 @@ export const getEditVideo = async (req, res) => {
   } = req;
   try {
     const video = await Video.findById(id);
-    res.render("editVideo", {
-      pageTitle: `Edit ${video.title}`,
-      video,
-    });
+    if (video.creator.toString() !== req.user.id) {
+      throw Error();
+    } else {
+      res.render("editVideo", {
+        pageTitle: `Edit ${video.title}`,
+        video,
+      });
+    }
   } catch (error) {
     res.redirect(routes.home);
   }
@@ -128,12 +132,16 @@ export const deleteVideo = async (req, res) => {
   } = req;
 
   try {
-    await Video.findOneAndRemove({
-      _id: id,
-    });
+    const video = await Video.findById(id);
+    if (video.creator.toString() !== req.user.id) {
+      throw Error();
+    } else {
+      await Video.findOneAndRemove({
+        _id: id,
+      });
+    }
   } catch (error) {
     console.log(error);
   }
   res.redirect(routes.home);
-  // res.render("deleteVideo", { pageTitle: "Delete Video"});
 };
